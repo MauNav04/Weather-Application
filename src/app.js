37430d15a6d340c0f17765230e3fc9d03d85639a
MauroN <mauronav006@gmail.com>
2023-08-07 19:00:16 -0600

    Port update

--- src/app.js
+++ src/app.js
@@ -4,8 +4,8 @@ const hbs = require('hbs')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-
 const app = express()
+const PORT = process.env.PORT || 3030;
 
 //Define paths for express config
 const publicDirPath = path.join(__dirname, '../public')
@@ -103,6 +103,6 @@ app.get('*', (req, res) => {
 })
 
 //Sets server to listen on port 3000
-app.listen(3000, () => {
-    console.log('Server is up in port 3000')
+app.listen(PORT, () => {
+    console.log(`Server is up in port ${PORT}`)
 })
